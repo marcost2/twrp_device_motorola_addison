8728eba17bf996ed55714fa3dd678b76aec09b6b
Jarl-Penguin <jarlpenguin@outlook.com>
2022-02-21 21:10:23 +0300

    addison: Inherit from embedded configuration
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- omni_addison.mk
+++ omni_addison.mk
@@ -15,7 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
